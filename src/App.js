250027052129b605ec668f4cc7061b9196da225c
LAPTOP-SNSEHSAR\ayush <ayushjain2205@gmail.com>
2021-05-25 01:28:22 +0530

    Hover effect added on Movie image tiles, fontawesome support added

--- src/App.js
+++ src/App.js
@@ -41,12 +41,38 @@ const config = {
   alwaysSearchOnInitialLoad: true,
 }
 
-const ResultTile = ({ titleField, result }) => (
+const ResultTile = ({ result }) => (
   <div className="result-tile">
-    {/* <h1>{result.title.raw}</h1> */}
     <img className="img-tile" src={result.poster.raw} alt="" />
     <div className="tile-overlay">
-      <div className="text">{result.title.raw}</div>
+      <div className="text">
+        <h1 className="movie-name">
+          {result.title.raw.replace(/ *\([^)]*\) */g, '')}
+        </h1>
+        <div className={`genre ${result.genre.raw}`}>{result.genre.raw}</div>
+        {result.year.raw}
+        <h5 className="cast">
+          {result.actors.raw.length > 40
+            ? result.actors.raw.slice(0, 37) + ' ...'
+            : result.actors.raw}
+        </h5>
+        <div className="links">
+          <a
+            className="link imdb"
+            href={`https://www.imdb.com/title/${result.imdb_id.raw}`}
+          >
+            <i class="fab fa-imdb"></i>
+          </a>
+          <a className="link wiki" href={result.wiki_link.raw}>
+            <i class="fab fa-wikipedia-w"></i>
+          </a>
+        </div>
+        <p className="summary">
+          {result.summary.raw.length > 100
+            ? result.summary.raw.slice(0, 97) + ' ...'
+            : result.summary.raw}
+        </p>
+      </div>
     </div>
   </div>
 )
